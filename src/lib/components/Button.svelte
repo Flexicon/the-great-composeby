d45cf02f851d9eb4e4cf30ca1acb21abfd59ace6
Mike Repec <mikerepec@gmail.com>
2024-09-08 22:13:17 +0200

    feat: add reusable button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	export let variant: 'primary' | 'secondary' | 'outline' = 'primary';
+</script>
+
+<button class={`variant-${variant}`} {...$$restProps}>
+	<slot />
+</button>
+
+<style lang="postcss">
+	button {
+		@apply inline-flex items-center justify-center whitespace-nowrap;
+		@apply h-10 px-4 py-2;
+		@apply rounded-md text-sm font-medium transition-colors;
+		@apply focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2;
+		@apply disabled:pointer-events-none disabled:opacity-50;
+	}
+
+	button.variant-primary {
+		@apply bg-yellow-500 text-black hover:bg-yellow-600;
+	}
+
+	button.variant-outline {
+		@apply border border-yellow-500 text-yellow-500;
+		@apply hover:bg-yellow-500 hover:text-black;
+	}
+</style>
