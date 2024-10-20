c0c5a265345ce69a4838bd22c7203a91c88bd9b3
Mike Repec <mikerepec@gmail.com>
2024-10-20 21:13:10 +0200

    refactor: adjust text sizing on service toggle

--- src/lib/components/ServiceToggle.svelte
+++ src/lib/components/ServiceToggle.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <label
-	class={`flex flex-col items-center justify-center py-2 px-3 bg-gray-800 rounded-md cursor-pointer ${$$props.class || ''}`}
+	class={`flex flex-col items-center justify-center p-2 bg-gray-800 rounded-md cursor-pointer ${$$props.class || ''}`}
 	class:opacity-50={!selected}
 	class:grayscale={!selected}
 >
@@ -20,5 +20,5 @@
 		on:change={(e) => dispatch('change', e.currentTarget.checked)}
 	/>
 	<img src={`/logos/${service.id}.svg`} alt={service.name} class="h-14 w-auto" />
-	<span class="text-xs mt-1">{service.name}</span>
+	<span class="text-xs text-center mt-1">{service.name}</span>
 </label>
